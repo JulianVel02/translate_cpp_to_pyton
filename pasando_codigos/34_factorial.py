477d82985d101890212532fbc91e9e1b2c563f01
JulianVel02 <julianvelazquezas@gmail.com>
2024-05-19 18:39:29 -0300

    Nuevos ejercicios

---
+++ pasando_codigos/34_factorial.py
@@ -0,0 +1,3 @@
+from funciones_generales import factorial_largo, leer_enteros
+nro = leer_enteros("Ingresá el numero del que queres saber el facotrial: ")
+print(f"El factorial de {nro}, es: {factorial_largo(nro)}")
